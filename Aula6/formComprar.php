6285fc29d4f73821958cb97362b82e53644e6f2f
SthephanySouza <101808324+SthephanySouza@users.noreply.github.com>
2022-10-05 01:00:14 -0300

    Update - teste
    exibição das informações dos produtos

--- Aula6/formComprar.php
+++ Aula6/formComprar.php
@@ -12,6 +12,28 @@
     session_start();
     include 'conexao.php';
     include 'nav.php';
+	
+	$codProd = $_GET['cd'];
+	$consulta = $cn->query("select * from vw_prod where codigo = '$codProd'");
+	$exibir = $consulta->fetch(PDO::FETCH_ASSOC);
 ?>
+
+<div class="container-fluid">
+
+	<div class="row align-items-start" style="padding-top: 20px; margin-left: 20px;">
+		
+		<div class="col-sm-4 col-sm-offset-1">
+			<div class="col-sm-5"><h4><?php echo $exibir['produto'];?></h4></div>
+		</div>
+		
+		<div class="col-sm-4 col-sm-offset-1">
+			<div class="col-sm-2"><h4><?php echo number_format($exibir['preco'], 2, ',', '.');?></h4></div>
+		</div>
+		
+		<div class="col-sm-4 col-sm-offset-1">
+			<button class="btn btn-lg btn-success">Comprar</button>
+		</div>
+	</div>
+</div>
 </body>
 </html>
