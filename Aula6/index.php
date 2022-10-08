6ae5cd644d6280cb576cfd750dd277e89f024264
FelipeOropeza <felipe.oropeza@etec.sp.gov.br>
2022-10-07 22:23:06 -0300

    Imagem Fake

--- Aula6/index.php
+++ Aula6/index.php
@@ -21,6 +21,7 @@
         <?php while($exibir=$consulta->fetch(PDO::FETCH_ASSOC)){?>
             <div class="col-sm-3">
                 <div><h5><b>Produto: <?php echo $exibir['produto']; ?></b></h5></div>
+                <div><svg class="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="250" height="250" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 250x250" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="37%" y="50%" fill="#aaa" dy=".3em">250x250</text></svg></div>
                 <div><h5>Preço: R$ <?php echo number_format($exibir['preco'],2, ',','.'); ?></h5></div>
                 <div class="text" style="margin-top:5px; margin-bottom:5px;">
                 <a class="btn btn-xl btn-block btn-success" href="formComprar.php?cd=<?php echo $exibir['codigo'];?>" role="button">Comprar</a>
